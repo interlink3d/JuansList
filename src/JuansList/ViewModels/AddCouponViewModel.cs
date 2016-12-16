fcf4291a0b6b0b707307b94ec10128b87cfd27c1
unknown <interlink3d@gmail.com>
2016-12-16 09:46:39 -0600

    created add, edit, and delete methods on the controller

---
+++ src/JuansList/ViewModels/AddCouponViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using JuansList.Models;
+
+namespace JuansList.ViewModels
+{
+    public class AddCouponViewModel
+    {
+        public Coupon coupon { get; set; }
+    }
+}
