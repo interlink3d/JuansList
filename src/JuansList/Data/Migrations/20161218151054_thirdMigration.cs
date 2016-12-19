02849683f719706ad1896c2881ba5888465c4890
unknown <interlink3d@gmail.com>
2016-12-18 22:00:16 -0600

    updated models to include collections of albums and categories

---
+++ src/JuansList/Data/Migrations/20161218151054_thirdMigration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JuansList.Data.Migrations
+{
+    public partial class thirdMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
